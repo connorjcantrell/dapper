d849a639522d9e52a900dd9f9454d92dc65de213
connorjcantrell <connorjcantrell@gmail.com>
2022-02-15 10:04:18 -0800

    Add documentation to init command

--- cmd/init.go
+++ cmd/init.go
@@ -24,12 +24,13 @@ var localInts uint
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize dapper in current working directory",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Long: `Initialize dapper in current working dirctory
+	Creates project a /.dapper and /src directory. 
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	A config.json file is generated inside /.dapper, which contains application
+	details that will be used to interact with the Algorand Blockchain
+
+	If boilerplate flag was used, code templates will be written inside /src`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fs, err := dapperfs.New()
 		if err != nil {
@@ -70,29 +71,4 @@ func init() {
 	flag.UintVar(&globalInts, "global-ints", 0, "Maximum number of integer values that may be stored in the global key/value store. Immutable.")
 	flag.UintVar(&localBytes, "local-byteslices", 0, "Maximum number of byte slices that may be stored in local (per-account) key/value stores for this app. Immutable.")
 	flag.UintVar(&localInts, "local-ints", 0, "Maximum number of integer values that may be stored in local (per-account) key/value stores for this app. Immutable.")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-/*
-TODO:
-`dappman init` will perform the following:
-    - Retrieve Current Working Directory (i.e. ~/algorand/projects/my-app)
-    - Create directories inside Current Working Directory
-		- ./.dappman
-        - ./src
-    	- ./public
-    - Create files in ./src
-        - approval_program.py
-    	- clear_state_program.py
-	- Create a file `config.json` inside `./.dappman`
-	    - `config.json` contains application specific information like Application ID, global/local state schemas, etc.
-*/
